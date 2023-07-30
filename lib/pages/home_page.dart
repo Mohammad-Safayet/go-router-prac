b72e11bf1535ad93453b455b137a51e710d801a5
Mohammad-Safayet <mohammad.safayet360@gmail.com>
2023-07-30 08:13:28 +0600

    implemented the appbar and navbar

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,24 +1,37 @@
 import 'package:flutter/material.dart';
 
 import 'package:go_router_prac/base/screen_base.dart';
+import 'package:go_router_prac/widgets/app_bar.dart';
+import 'package:go_router_prac/widgets/bottom_nav_bar.dart';
 
 class HomeScreen extends BaseView {
   HomeScreen({super.key});
 
   @override
   PreferredSizeWidget? appBar(BuildContext context) {
-    return null;
+    return const ApplicationBar(
+      child: Text("go_router prac"),
+    );
   }
 
   @override
   Widget body(BuildContext context) {
     return Container(
-      color: Colors.amber,
-      child: const Center(
+      color: Theme.of(context).colorScheme.surface,
+      child: Center(
         child: Text(
           "Home",
+          style: TextStyle(
+            fontSize: 25.0,
+            color: Theme.of(context).colorScheme.onSurface,
+          ),
         ),
       ),
     );
+  }
+
+  @override
+  Widget? bottomNavigationBar() {
+    return const BottomNavBar();
   }
 }
