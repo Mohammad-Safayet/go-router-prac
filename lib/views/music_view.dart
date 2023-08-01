74e77d6a0d21cba3168776af74a686ba65dbcf5d
Mohammad-Safayet <mohammad.safayet360@gmail.com>
2023-08-01 06:43:06 +0600

    updated music view

---
+++ lib/views/music_view.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class MusicView extends StatelessWidget {
+  MusicView({super.key});
+
+  final Map<String, String> products = {
+    "In The End": "Linkin Park",
+    "Numb": "Linkin Park",
+    "My Name Is": "Eminem",
+    "Lonely": "Akon",
+    "Sorry, Blame It On Me": "Akon",
+    "Fireflies": "Owl City",
+    "Boulevard Of Broken Dreams": "Green Day",
+    "Wake Me Up When September Ends": "Green Day",
+    "They Don’t Care About Us": "Michael Jackson",
+  };
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Theme.of(context).colorScheme.background,
+      child: ListView.builder(
+        itemCount: products.length,
+        itemBuilder: (context, index) {
+          final songs = products.keys.toList();
+          final artists = products.values.toList();
+          return ListTile(
+            title: Text(
+              songs[index],
+              style: const TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            subtitle: Text(
+              artists[index],
+              style: const TextStyle(
+                fontSize: 14,
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
